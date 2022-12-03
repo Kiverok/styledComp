0bc9bfd52357df9f136d59395aec9e7f801f2aae
Kiverok <kiver566@ukr.net>
2022-12-03 16:14:29 +0200

    styled

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,24 +1,26 @@
 import PropTypes from 'prop-types';
 import { BsAlarm } from 'react-icons/bs';
 import { HiOutlineChartPie, HiOutlineChartBar } from 'react-icons/hi';
+import { RecipeInfo, InfoBlock, Badge, BadgeList } from './Recipe.styled';
 
 
-export const Recipe = ({ recipe: {Name, time, servings, calories} }) => {
+
+export const Recipe = ({ recipe: {Name, time, servings, calories, difficulty} }) => {
     return (
-        <div style={{ border: '1px solid black'}}>
+        <div>
             <h2>{Name}</h2>
-            <div>
-                <div> <BsAlarm/> {time} min</div>
-                <div> <HiOutlineChartPie/> {servings} servings</div>
-                <div> <HiOutlineChartBar/> {calories} calories</div>
-            </div>
+            <RecipeInfo>
+                <InfoBlock> <BsAlarm size={24} /><span>{time} min</span></InfoBlock>
+                <InfoBlock> <HiOutlineChartPie size={24}/> <span>{servings} servings</span></InfoBlock>
+                <InfoBlock> <HiOutlineChartBar size={24}/><span>{calories} calories</span></InfoBlock>
+            </RecipeInfo>
             <div>
                 <h3>Difficulty</h3>
-                <div>
-                <span>Easy</span>
-                <span>Medium</span>
-                <span>Hard</span>
-                </div>
+                <BadgeList>
+                <Badge difficulty={difficulty}></Badge>
+                <Badge>Medium {difficulty === 'medium' && 'active'}</Badge>
+                <Badge>Hard {difficulty === 'hard' && 'active'}</Badge>
+                </BadgeList>
             </div>
         </div>
     )
@@ -28,6 +30,7 @@ recipe: PropTypes.shape({
 name: PropTypes.string.isRequired,
 time: PropTypes.string.isRequired,
 servings: PropTypes.number.isRequired,
-calories: PropTypes.number.isRequired
+calories: PropTypes.number.isRequired,
+difficulty:PropTypes.oneOf(['easy', 'medium', 'hard']).isRequired,
 }).isRequired
 };
